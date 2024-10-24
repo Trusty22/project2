6e72d7234e443c4a8a3f542f730103ed19ca4e5e
Mandeep Masoun <mandeepmasoun@yahoo.com>
2024-10-23 17:29:58 -0700

    done

--- sthread.cpp
+++ sthread.cpp
@@ -32,16 +32,16 @@
     thr_queue.push(cur_tcb);                               \
   }
 
-#define sthread_yield()                                        \
-  {                                                            \
-    if (alarmed) {                                             \
-      alarmed = false;                                         \
-      if (setjmp(cur_tcb->env_) == 0) {                        \
-        memcpy(cur_tcb->sp_, cur_tcb->stack_, cur_tcb->size_); \
-        capture();                                             \
-        longjmp(scheduler_env, 1);                             \
-      }                                                        \
-    }                                                          \
+#define sthread_yield()                                      \
+  {                                                          \
+    if (alarmed) {                                           \
+      alarmed = false;                                       \
+      if (setjmp(cur_tcb->env_) == 0) {                      \
+        capture();                                           \
+        longjmp(scheduler_env, 1);                           \
+      }                                                      \
+      memcpy(cur_tcb->sp_, cur_tcb->stack_, cur_tcb->size_); \
+    }                                                        \
   }
 
 #define sthread_init()                                     \
